07b36d25c1285392af48b8f863c4b215892d0add
abrassiv3 <abrassiv3@gmail.com>
2023-10-17 16:21:05 -0700

    2tes2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function name
+ * @s: parameters
+ * return length of string
+ */
+void _strlen(char *s)
+{
+	int v;
+	int sum = 0;
+
+	for (v = 0; s[i] != '\0'; v++)
+		sum++;
+	return (sum);
+}
