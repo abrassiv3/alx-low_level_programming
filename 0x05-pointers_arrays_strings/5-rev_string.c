000a4ae21caa035d4832ea946e7bfb10c21ca943
abrassiv3 <abrassiv3@gmail.com>
2023-10-18 03:39:23 -0700

    push

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * rev_string - function name
+  * @s: function parameter
+  *
+  */
+void rev_string(char *s);
+{
+	int d;
+	int c = 0;
+
+	for (d = 0; s[d] != '\0'; d++)
+		c++;
+	for (d = c - 1; d >= 0; d--)
+		_putchar(s[d]);
+	_putchar('\n');
+}
