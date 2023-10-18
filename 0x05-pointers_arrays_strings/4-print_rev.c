ba4efd7062071977a69f15ccdfde9f3c202394c0
abrassiv3 <abrassiv3@gmail.com>
2023-10-17 17:12:54 -0700

    ihy3betty

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_rev - function name
+  *@s: function parameter
+  */
+void print_rev(char *s)
+{
+	int d;
+	int c = 0;
+
+	for (d = 0; s[d] != '\0'; d++)
+		c++;
+	for (d = c; s[d] >= 0; d--)
+		putchar(s[d]);
+	_putchar('\n');
+}
