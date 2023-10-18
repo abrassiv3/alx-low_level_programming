1d8fe4a204bace29e05648c5257e24575d71a477
abrassiv3 <abrassiv3@gmail.com>
2023-10-18 04:19:03 -0700

    ooredoo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,15 @@
 #include "main.h"
 /**
-  * rev_string - function name
-  * @s: function parameter
-  *
+  *rev_string - function name
+  *@s: function parameter
   */
 void rev_string(char *s)
 {
-	int i;
-	int sum = 0;
+	int d;
+	int c = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		sum++;
-	for (i = 0; i < sum / 2; i++)
-		char j;
-
-		j = s[i];
-			s[i] = s[sum - 1 - i];
-			s[sum - 1 - i] = j;
+	for (d = 0; s[d] != '\0'; d++)
+		c++;
+	for (d = c - 1; d >= 0; d--)
+		_putchar(s[d]);
 }
