e25fa81cd4c2b1d1393e1fe1d75c21483665b30f
abrassiv3 <abrassiv3@gmail.com>
2023-10-17 18:42:56 -0700

    Mere

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,8 @@ void puts2(char *str)
 {
 	int d;
 
-	for (d = 0; str[d] != '\0'; d = d + 2)
-		_putchar(str[d]);
+	for (d = 0; str[d] != '\0'; d++)
+		if (d % 2 == 0)
+			_putchar(str[d]);
 	_putchar('\n');
 }
