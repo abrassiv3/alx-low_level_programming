f2c2cee6ae1bcf911f54834db9e17cadeec14612
abrassiv3 <abrassiv3@gmail.com>
2023-10-17 18:31:27 -0700

    ihy3betty

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *puts2 - function name
+  *@str: function parameter
+  */
+void puts2(char *str)
+{
+	int d;
+
+	for (d = 0; str[d] != '\0'; d + 2)
+		_putchar(str[d]);
+	_putchar('\n');
+}
