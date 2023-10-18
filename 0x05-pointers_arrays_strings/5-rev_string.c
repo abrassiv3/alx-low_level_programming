336fd17f4e144c81befb2e2ecee465950112e1d6
abrassiv3 <abrassiv3@gmail.com>
2023-10-18 09:05:09 -0700

    defer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,8 +10,10 @@ void rev_string(char *s)
 
 	for (d = 0; s[d] != '\0'; d++)
 		c++;
-	for (d = o; d < c / 2; d++)
-		char j = s[d];
+	for (d = 0; d < c / 2; d++)
+		char j;
+
+		j = s[d];
 			s[d] = s[c - 1 - d];
-			s[c - 1 - i] = j;
+			s[c - 1 - d] = j;
 }
