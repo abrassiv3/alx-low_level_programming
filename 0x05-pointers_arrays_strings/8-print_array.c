bea64df0914054fdb0c3084e3bf7b5e0eb43027c
abrassiv3 <abrassiv3@gmail.com>
2023-10-17 19:10:51 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_array - function name
+  *@a: function parameter
+  *@n: function parameter
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d", a[i]);
+			if (i < n - 1)
+				printf(", ");
+	printf("\n");
+}
