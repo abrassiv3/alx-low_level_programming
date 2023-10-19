62dc3b5b44928dd8ce26f0f29aef10428fcfac97
abrassiv3 <abrassiv3@gmail.com>
2023-10-19 14:00:32 -0700

    PUSH

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *_strcat - function name
+  *@dest: function parameter
+  *@src: function parameter
+  */
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		c++;
+	for (i = 0; dest[i] < c; i++)
+		_putchar(dest[i])
+			if (dest[i] == c)
+				while (str[i] != '\0')
+					_putchar(str[i]);
+	return(*dest);
+}
