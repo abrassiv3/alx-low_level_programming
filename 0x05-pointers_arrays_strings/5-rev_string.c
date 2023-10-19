3f6a86680dbf7ac731da0fa41543025b634d3dd1
abrassiv3 <abrassiv3@gmail.com>
2023-10-19 00:14:24 -0700

    PUSH

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,15 +5,17 @@
   */
 void rev_string(char *s)
 {
-	int d;
-	int c = 0;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	for (d = 0; s[d] != '\0'; d++)
-		c++;
-	for (d = 0; d < c / 2; d++)
-		char j;
-
-		j = s[d];
-			s[d] = s[c - 1 - d];
-			s[c - 1 - d] = j;
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
