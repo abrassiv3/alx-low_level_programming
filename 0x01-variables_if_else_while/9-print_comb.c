cf5f02fb2cdbefe7bb0f647a9df4da2e68645bea
abrassiv3 <abrassiv3@gmail.com>
2023-10-23 10:42:06 -0700

    TREFUEGO

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - function name
+  *Return: 0
+  */
+int main(void)
+{
+	int r;
+
+	for (r = '0'; r <= '8'; r++)
+		putchar('r, ');
+	putchar('9');
+
+	return (0);
+}
+
