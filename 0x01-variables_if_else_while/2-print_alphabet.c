90b31e44271462522170cace88d492f9bd40d387
abrassiv3 <abrassiv3@gmail.com>
2023-10-23 09:15:51 -0700

    TREFUEGO

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - function name
+  * Return: 0
+  */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+	return (0);
+}
