1cf273a41bfb74d8cd825878d3a5e185ba4a79c3
abrassiv3 <abrassiv3@gmail.com>
2023-10-23 10:16:13 -0700

    TREFUEGO

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - function name
+  *Return: 0
+  */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
