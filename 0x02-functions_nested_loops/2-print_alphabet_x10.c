f298f30dfc8a0b128acfd942d3986cf6d575e14d
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-10-24 14:01:15 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,8 @@ void print_alphabet_x10(void)
 	int e = 0;
 
 	while (e++ <= 9){
-		for (a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++){
 			_putchar(a);
 		if (a == 'z'){
-			_putchar('\n'); }}
+			_putchar('\n'); }}}
 }
