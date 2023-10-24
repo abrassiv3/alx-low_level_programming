c1b9f4798064a7aee9b0c016c0f1910dd6d8560b
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-10-24 11:07:19 +0300

    Update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *_islower - function name
+  *@c - function parameter
+  *Return: 0
+  */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+_putchar(0);
+else
+_putchar(1);
+}
