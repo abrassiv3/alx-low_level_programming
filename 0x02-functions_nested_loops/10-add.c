b425678f829956e8b4e7a0cd1300ea42a61a1cd2
abrassiv3 <abrassiv3@gmail.com>
2023-10-24 11:57:39 -0700

    TREFUEGO

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function name
+ * @num1: function parameter
+ * @num2: function parameter.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
