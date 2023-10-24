00c018aa98d08a92c78cc32f250315c473f9c0ea
abrassiv3 <abrassiv3@gmail.com>
2023-10-24 09:39:03 -0700

    TREFUEGO

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *print_to_98 - function name
+  *@n: function parameter
+  *Return = 0
+  */
+void print_to_98(int n)
+{
+	int r;
+
+	if (n == 98)
+		_putchar(n);
+	else if (n < 98)
+		for (r = n; r < 98; r++)
+		{
+			_putchar(r);
+			_putchar(',');
+			_putchar(' ');
+			if (r == 97)
+				r++;
+				_putchar(r);
+		}
+}
