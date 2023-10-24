0512677215fea8f2a0d465a7d44789a1497adff9
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-10-24 11:27:11 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,5 +6,16 @@
   */
 int print_sign(int n)
 {
-if (r < 0)
-print_sign
+if (r < 0){
+return (-1);
+_putchar('-');
+}
+else if (r == 0){
+return (0);
+_putchar(0);
+}
+else{
+return (1);
+_putchar('+');
+}
+}
