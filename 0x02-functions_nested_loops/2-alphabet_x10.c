9c8774a88b7df8bea4d1f9670e728647f14bed21
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-10-24 10:21:50 +0300

    Update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - function name
+  *Return: 0
+  */
+void print_alphabet_x10(void)
+{
+char a;
+int e;
+
+for (e = 0; e <= 9; e++)
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+
+_putchar('/n');
+} 
