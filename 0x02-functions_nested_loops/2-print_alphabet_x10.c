9d6c0075e37b6d83abd816baf0233f991d53a2f3
abrassiv3 <abrassiv3@gmail.com>
2023-10-24 04:48:34 -0700

    TREFUEGO

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,15 @@ void print_alphabet_x10(void)
 	char a;
 	int e = 0;
 
-	while (e++ <= 9) {
-		for (a = 'a'; a <= 'z'; a++) {
+	while (e++ <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
 			_putchar(a);
-		if (a == 'z') {
-			_putchar('\n'); }}}
+		if (a == 'z')
+		{
+			_putchar('\n');
+		}
+		}
+	}
 }
