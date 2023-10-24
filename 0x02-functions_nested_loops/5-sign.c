1d76cb9c19c3a52ece53fe70ea0c787c2e5cf584
abrassiv3 <abrassiv3@gmail.com>
2023-10-24 03:18:24 -0700

    trefuego

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,18 @@
 int print_sign(int r)
 {
 	if (r < 0)
+	{
 		_putchar('-');
 		return (1);
+	}
 	else if (r == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	else if (r > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 }
