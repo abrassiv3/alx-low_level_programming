532c3782ccef5fe81376d502dd93d27c1d20c30e
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-10-24 09:08:59 +0300

    Update 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_alphabet - function name
+  *Return: 0
+  */
+void print_alphabet(void);
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+return (0);
+}
