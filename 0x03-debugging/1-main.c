eca2ec08ebdd8018f88d86f347a8353a3912e389
abrassiv3 <abrassiv3@gmail.com>
2023-10-30 08:54:01 -0700

    TREFUEGO

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-	int i;
+	/*int i;*/
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+	/*i = 0;*/
 
-	while (i < 10)
+	/*while (i < 10)*/
 	{
-	putchar(i);
-	/* There is no increment after the function putchar. */
+	/*putchar(i);*/
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
