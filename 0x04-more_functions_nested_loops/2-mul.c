7a1c3f5df547cf7b8f7d6d9212166faf9b275d08
abrassiv3 <abrassiv3@gmail.com>
2023-10-31 07:52:49 -0700

    TREFUEGO

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *mul - Function name
+  *@a: function parameter
+  *@b: function parameter
+  *Return: multiplication results
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
