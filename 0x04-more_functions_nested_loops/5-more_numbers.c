22e0a70b669478ebdee965c2582fb19ae79ed95d
abrassiv3 <abrassiv3@gmail.com>
2023-10-31 10:12:43 -0700

    TREFUEGO

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-  *more_numbers - Function name
-  *Return: 0
+  *more_numbers - function name
   */
+
 void more_numbers(void)
 {
 	int e;
-	char f, g;
-	char h = 1;
+	char i;
 
-	for (e = 0; e <= 10; e++)
-	       for (f = '0'; f <= '9'; f++)
-			_putchar(f);
-			if (f == '9')
-				for (g = '1'; g <= '4'; g++)
-					_putchar(h);
-					_putchar(g);
+	for (e = 0; e <= 9; e++)
+	{
+		for (i = '0'; i <= '9'; i++)
+			putchar(i);
+			printf("1011121314");
+		putchar('\n');
+	}
+	return (0);
 }
