911b918ddcd7d9328911f6c0e2e7fbcbe0f6a891
abrassiv3 <abrassiv3@gmail.com>
2023-10-31 09:39:19 -0700

    TREFUEGO

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,7 @@ void more_numbers(void)
 	for (e = 0; e <= 10; e++)
 	       for (f = '0'; f <= '9'; f++)
 			_putchar(f);
-			if (f == 9)
+			if (f == '9')
 				for (g = '1'; g <= '4'; g++)
 					_putchar(h);
 					_putchar(g);
