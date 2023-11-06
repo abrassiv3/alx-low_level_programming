dce101e82611b4b033adfbc1161b8024ca786c8f
abrassiv3 <abrassiv3@gmail.com>
2023-11-06 06:35:28 -0800

    TREFUEGO

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,3 +6,13 @@
   *Return: 0
   */
 void print_square(int size)
+{
+	int y;
+	int f;
+
+	for (y = 0; y < size; y++)
+		for (f = 0; f < size; f++)
+			_putchar('#');
+			if (f == size - 1)
+				_putchar('\n');
+}
