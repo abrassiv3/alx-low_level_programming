f2fde571d26992980d57de156099e21d15db1d08
abrassiv3 <abrassiv3@gmail.com>
2023-11-06 13:57:35 -0800

    TREFUEGO

--- 0x08-recursion/2-main.c
+++ 0x08-recursion/2-main.c
@@ -1,10 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 /**
   *main - check code
   *Return: 0
   */
 int main(void)
 {
-	_print_rev_recursion("\nis iti n reverse?");
+	int e;
+
+
+	e = _strlen_recursion("D E s t riegb,s");
+	printf("%d\n", e);
 	return (0);
 }
