0ab18359c4f378eea477fc1572d7a3d6cb416512
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-11-06 11:38:54 +0300

    Update 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+
+int r;
+
+for (r 0; r <= 100; r++)
+if (r > 0 && r % 3 == 0) 
+printf("Fizz ");
+
+else if (r > 0 && r%5 == 0)
+printf("Buzz ");
+
+else
+printf("%d ", r);
+
+return(0);
+}
