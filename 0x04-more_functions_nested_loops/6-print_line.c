f59749f65a0dc4877ca744a462ecc375a7c4fe6d
abrassiv3 <128499185+abrassiv3@users.noreply.github.com>
2023-11-06 11:00:06 +0300

    Update 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *print_line - function name
+  *@n: function parameters
+  *Return: 0
+  */
+
+void print_line(int n)
+{
+    int r;
+
+for (r = 0; r <= n; r++)
+_putchar('_');
+
+}
