6004783074e15b1a0c69401f2562ae869ffe8a50
abrassiv3 <abrassiv3@gmail.com>
2023-11-07 06:36:57 -0800

    TREFUEGO

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  *sqrchk - function name
+  *@a: function parameter
+  *@b: function parameter
+  *Return: sqrchk
+  */
+int sqrchk(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > 0)
+	{
+		return (-1);
+	}
+	return (sqrchk(a, (b + 1)));
+/**
+  *_sqrt_recursion - function name
+  *@n: function parameter
+  *Return: -1 if not natural, n if natural square root.
+  */
+int _sqrt_recursion(int n)
+{
+	return (sqrchk(n, 0));
+}
