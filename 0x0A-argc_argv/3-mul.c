202b81f5d31bd756ef262f4e9695781422c1b285
abrassiv3 <abrassiv3@gmail.com>
2023-11-08 07:28:12 -0800

    TREFUEGO

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
   * main - function name
   * @argc: function parameter
@@ -7,17 +8,12 @@
   */
 int main(int argc, char **argv)
 {
-	int n, p, q, r;
+	int p, q, r;
 
 	if (argc > 2 && argc < 4)
 	{
-		for (n = 1; n < argc; n++)
-		{
-			*argv[n] = (*argv[n] - '0');
-		}
-
-		p = *argv[1];
-		q = *argv[2];
+		p = atoi(argv[1]);
+		q = atoi(argv[2]);
 		r = q * p;
 		printf("%d\n", r);
 	}
