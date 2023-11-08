5a50ff259ab9eb88f042e395dac6ff3d3fa9aeab
abrassiv3 <abrassiv3@gmail.com>
2023-11-08 00:54:32 -0800

    TREFUEGO

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
   * main - function name
-  * @argc: function parameter 
+  * @argc: function parameter
   * @argv: function parameter
   * Return: 0
   */
 int main(int argc, char **argv)
 {
 	int n;
-       
+
 	for (n = 0; n < argc; n++)
 	{
 		printf("%s\n", argv[n]);
