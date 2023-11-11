d536e8559156c278b2a67e13a8532113c1a53914
abrassiv3 <abrassiv3@gmail.com>
2023-11-11 01:21:06 -0800

    TREFUEGO

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
   *sum_them_all - function name
@@ -6,3 +7,24 @@
   *Return: sum unless n = 0
   */
 int sum_them_all(const unsigned int n, ...)
+{
+	va_list x;
+	unsigned int j, result = 0;
+
+	va_start(x, n);
+
+	for (j = 0; j < n; j++)
+	{
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			result += va_arg(x, const unsigned int);
+		}
+	}
+
+	va_end(x);
+	return (result);
+}
