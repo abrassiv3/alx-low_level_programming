bce51734692b9392c733d92dc1246ca3f56ecb82
abrassiv3 <abrassiv3@gmail.com>
2023-11-12 11:15:54 -0800

    TREFUEGO

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,9 +22,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (name == NULL)
 		{
 			PRINTF("(nil)");
-		printf("%s", name);
-		if (loop != (n - 1) && separator != NULL)
-			printf("%s", separator);
+		}
+		else
+		{
+			printf("%s", name);
+			if (loop != (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
 	}
 	va_end(r);
 	printf("\n");
