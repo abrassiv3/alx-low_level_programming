3f6c402361f5e8f80de408901e2075705e307fb2
abrassiv3 <abrassiv3@gmail.com>
2023-11-21 10:20:07 -0800

    TREFUEGO

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  * struct dog - a new type describing a dog
+  * @name: dog name
+  * @owner: dog owner
+  * @age: dog age
+  */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
